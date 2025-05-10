25cc2aa3d27daeb0d53ec70bcdd324ceead95404
Oleg <starostinoleg370@gmail.com>
2025-05-10 13:12:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ function sortArrayAscending(numbers) {
 const myArray = [3, 1, 4, 1, 5, 9, 2, 6];
 const sortedArray = sortArrayAscending(myArray);
 console.log(sortedArray); // Output: [1, 1, 2, 3, 4, 5, 6, 9]
+console.log('-=-=-=-=-=')
